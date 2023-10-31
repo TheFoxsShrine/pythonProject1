b04cf3a04dfd4a9a19e8a3dd3f5411fb2dc42c3e
Анастасия Белова <nastya_belova_04@mail.ru>
2023-10-31 21:24:38 +0500

    log + cos

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0, 2 * np.pi, 100) 
-y = np.log(x) * np.cos(x) 
+y = np.log(x) + np.cos(x) 
 
 plt.plot(x, y)
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('График функции y = log(x) * cos(x)')
+plt.title('График функции y = log(x) + cos(x)')
 
 plt.show()
